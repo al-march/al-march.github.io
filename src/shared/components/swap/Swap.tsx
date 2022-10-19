4d06178427be904e9aa23fccc59887e7d426dc7e
Alexander <valverin95@gmail.com>
2022-10-20 02:15:57 +0300

    feat: add Swap component

---
+++ src/shared/components/swap/Swap.tsx
@@ -0,0 +1,58 @@
+import {createEffect, createSignal, ParentProps} from 'solid-js';
+
+export type SwapProps = {
+  isOn?: boolean;
+  class?: string;
+};
+
+const SwapBase = (props: ParentProps<SwapProps>) => {
+  const [isOn, setIsOn] = createSignal(props.isOn);
+
+  createEffect(() => {
+    setIsOn(props.isOn);
+  });
+
+  function toggle() {
+    setIsOn(!isOn());
+  }
+
+  return (
+    <div
+      class="swap swap-rotate"
+      classList={{
+        [props.class || '']: !!props.class,
+        'swap-active': isOn(),
+      }}
+      onClick={toggle}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const SwapOn = (props: ParentProps<{class?: string}>) => {
+  return (
+    <div
+      class="inline-flex swap-on"
+      classList={{[props.class || '']: !!props.class}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const SwapOff = (props: ParentProps<{class?: string}>) => {
+  return (
+    <div
+      class="inline-flex swap-off"
+      classList={{[props.class || '']: !!props.class}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export const Swap = Object.assign(SwapBase, {
+  On: SwapOn,
+  Off: SwapOff,
+});
