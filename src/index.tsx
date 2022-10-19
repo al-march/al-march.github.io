858effa2b50c097ddff3ce4a56512b3bdb05103b
Alexander <valverin95@gmail.com>
2022-10-20 00:35:43 +0300

    update config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 /* @refresh reload */
-import './index.css';
-import { render } from 'solid-js/web';
-
 import App from './App';
+import './index.css';
+import {render} from 'solid-js/web';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
